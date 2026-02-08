661257ec1dc358a826e83be7239a192e9f099293
legion80 <legion80@users.noreply.github.com>
2026-02-08 19:54:19 +0000

    deploy: 31b8a423e23ff47891c642d4a46ffd9c2cad0895

--- assets/main.js
+++ assets/main.js
@@ -2,3 +2,13 @@ function playerClicked(playerNode) {
     const playerType = playerNode.dataset.playertype;
     playerNode.closest(".audio-player").classList.add(playerType);
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    const NUMBER_OF_STYLES = 4;
+    var current = new Date();
+    var yearStart = new Date(current.getFullYear(), 0, 1);
+    var timeDifference = current - yearStart;
+    var oneDayInMilliseconds = 1000 * 60 * 60 * 24;
+    var daysPassed = Math.ceil(timeDifference / oneDayInMilliseconds + 1);
+    document.body.classList.add("style-" + (daysPassed % NUMBER_OF_STYLES));
+});
